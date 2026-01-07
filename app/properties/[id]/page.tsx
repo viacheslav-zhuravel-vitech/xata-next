a27df8765590db58cf399ac891aa281e765a4c6a
ViacheslavZhuravel <jurik199307@gmail.com>
2026-01-07 20:47:45 +0100

    enhance property details layout and add back navigation

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -6,6 +6,7 @@ import {
   FaMapMarker,
   FaMoneyBill,
   FaRulerCombined,
+  FaAngleLeft,
 } from "react-icons/fa";
 import Link from "next/link";
 import { connectDB } from "@/config/database";
@@ -21,25 +22,42 @@ const PropertyPage: FC<PropertyPageProps> = async ({ params }) => {
   const { id } = await params;
   await connectDB();
   const property: PropertyType = await Properties.findById(id).lean();
+  console.log(property);
   return (
-    <>
-      <h1 className="text-2xl font-bold mb-6">{property.name}</h1>
-      <Image
-        src={`/images/properties/${property.images[0]}`}
-        alt=""
-        width="0"
-        height="0"
-        sizes="100vw"
-        className="w-full h-auto rounded-t-xl max-w-[1200px] m-auto"
-      />
-      <div className="p-4">
-        <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">{property.type}</div>
-        </div>
-        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-green-600 font-bold text-right md:text-center lg:text-right">
+    <div className="p-6">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold mb-6">{property.name}</h1>
+        <Link
+          href="/properties"
+          className="text-green-600 font-bold text-2xl flex items-center gap-1 mb-6"
+        >
+          <FaAngleLeft /> Back
+        </Link>
+      </div>
+      <div className="relative">
+        <Image
+          src={`/images/properties/${property.images[0]}`}
+          alt=""
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-auto rounded-t-xl max-h-[650px] m-auto object-cover"
+        />
+        <h3 className="absolute top-2.5 right-2.5 bg-white px-4 py-2 rounded-lg text-green-600 font-bold text-right md:text-center lg:text-right">
           {getRateDisplay(property.rates)}
         </h3>
-
+      </div>
+      <div className="p-4">
+        <div className="flex justify-between text-gray-600">
+          <div>{`Type: ${property.type}`}</div>
+          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
+            <span>Location:</span>
+            <FaMapMarker className="text-green-600 mt-1" />
+            <span className="text-green-600">
+              {property.location.city} {property.location.state}
+            </span>
+          </div>
+        </div>
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
             <FaBed className="lg:inline" /> {property.beds}{" "}
@@ -63,26 +81,8 @@ const PropertyPage: FC<PropertyPageProps> = async ({ params }) => {
             <FaMoneyBill className="lg:inline" /> Monthly
           </p>
         </div>
-
-        <div className="border border-gray-100 mb-5"></div>
-
-        <div className="flex flex-col lg:flex-row justify-between mb-4">
-          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-            <FaMapMarker className="text-green-600 mt-1" />
-            <span className="text-green-600">
-              {" "}
-              {property.location.city} {property.location.state}{" "}
-            </span>
-          </div>
-          <Link
-            href={`/properties/${property._id}`}
-            className="h-[36px] bg-green-600 hover:scale-105 transition-transform duration-300 ease-in-out text-white px-4 py-2 rounded-lg text-center text-sm"
-          >
-            Details
-          </Link>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default PropertyPage;
--- components/PropertyCard/PropertyCard.tsx
+++ components/PropertyCard/PropertyCard.tsx
@@ -51,7 +51,7 @@ const PropertyCard: FC<PropertyCardProps> = ({ property }) => {
           <div className="text-gray-600">{property.type}</div>
           <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
-        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-green-600 font-bold text-right md:text-center lg:text-right">
+        <h3 className="absolute top-2.5 right-2.5 bg-white px-4 py-2 rounded-lg text-green-600 font-bold text-right md:text-center lg:text-right">
           {getRateDisplay(property.rates)}
         </h3>
 
@@ -91,7 +91,7 @@ const PropertyCard: FC<PropertyCardProps> = ({ property }) => {
           </div>
           <Link
             href={`/properties/${property._id}`}
-            className="h-[36px] bg-green-600 hover:scale-105 transition-transform duration-300 ease-in-out text-white px-4 py-2 rounded-lg text-center text-sm"
+            className="h-9 bg-green-600 hover:scale-105 transition-transform duration-300 ease-in-out text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
           </Link>
